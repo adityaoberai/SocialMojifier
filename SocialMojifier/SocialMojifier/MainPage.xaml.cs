92e9345a88b6da386c22041af99c3e82ba29b181
Aditya Oberai <adityaoberai1@gmail.com>
2021-07-30 19:46:29 +0530

    Add project files.

---
+++ SocialMojifier/SocialMojifier/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Plugin.Media;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SocialMojifier
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void CaptureImage_Clicked(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("No Camera", ":( No camera available.", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+                Directory = "Sample",
+                Name = "test.jpg",
+                SaveToAlbum = true
+            });
+
+            if (file == null)
+            {
+                return;
+            }
+
+            await DisplayAlert("File Location", file.Path, "OK");
+
+            Capture.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                return stream;
+            });
+        }
+    }
+}
